4d4fe5fcc6310d62b2388d3bcbc33dbbc55ff1a0
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2022-01-24 22:30:40 -0700

    requiring newer tshirt for ROEBA subtraction

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ["tshirt",
-                "jwst"]
+                "jwst >= 0.1dev9"]
 
 test_requirements = ['pytest>=3', ]
 

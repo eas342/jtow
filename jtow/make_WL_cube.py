73d2014e36cb5b802e04c69f8301887cbb4ee6f4
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2023-02-18 23:40:42 -0700

    changing to object for easier variable passing

--- jtow/make_WL_cube.py
+++ jtow/make_WL_cube.py
@@ -12,46 +12,60 @@ from copy import deepcopy
 relFileSearch = 'Documents/jwst/flight_data/proc/01274/nrca3_fenrir_proc_002_groebak/split_output/*.fits'
 defFileSearch = os.path.join(os.environ['HOME'],relFileSearch)
 
-def make_WL_cube(fileSearch=defFileSearch,
-                 saveFits=False,xLim=[950,1150]):
-    
-    fileList = np.sort(glob.glob(fileSearch))
-    nImg = len(fileList)
-    
-    outDir = os.path.join(os.path.split(fileSearch)[0],'WL_cube')
-    firstPath = fileList[0]
-    firstHead = fits.getheader(firstPath)
-    
-    if os.path.exists(outDir) == False:
-        os.mkdir(outDir)
-    
-    excpMessage = "No default coord set for {} {} {}".format(firstHead['FILTER'],firstHead['SUBARRAY'],
-                                                             firstHead['DETECTOR'])
-    if firstHead['FILTER'] == 'WLP4':
-        if firstHead['SUBARRAY'] == 'SUBGRISM64':
-            if firstHead['DETECTOR'] == 'NRCA3':
-                x1, x2, y1, y2 = 1025, 1105, 0, 64
-            elif firstHead['DETECTOR'] == 'NRCA1':
-                x1, x2, y1, y2 = 1756, 1836, 0, 64
+class wlcubeMake(object):
+    def __init__(self,fileSearch=defFileSearch):
+        """
+        Initialize WL cube object
+        """
+        self.fileList = np.sort(glob.glob(fileSearch))
+        self.nImg = len(self.fileList)
+        
+        self.outDir = os.path.join(os.path.split(fileSearch)[0],'WL_cube')
+        self.firstPath = self.fileList[0]
+        self.firstHead = fits.getheader(self.firstPath)
+        
+        if os.path.exists(self.outDir) == False:
+            os.mkdir(outDir)
+        
+
+    def make_WL_cube(self):
+        firstHead = self.firstHead
+        excpMessage = "No default coord set for {} {} {}".format(firstHead['FILTER'],firstHead['SUBARRAY'],
+                                                                firstHead['DETECTOR'])
+        if firstHead['FILTER'] == 'WLP4':
+            if firstHead['SUBARRAY'] == 'SUBGRISM64':
+                if firstHead['DETECTOR'] == 'NRCA3':
+                    x1, x2, y1, y2 = 1025, 1105, 0, 64
+                elif firstHead['DETECTOR'] == 'NRCA1':
+                    x1, x2, y1, y2 = 1756, 1836, 0, 64
+                else:
+                    raise Exception(excpMessage)
             else:
                 raise Exception(excpMessage)
         else:
             raise Exception(excpMessage)
-    else:
-        raise Exception(excpMessage)
-    
-    wlCube = np.zeros([nImg,y2-y1,x2-x1])
-    
-    for ind in tqdm.tqdm(np.arange(nImg)):
-        oneFile = fileList[ind]
         
-        HDUList = fits.open(oneFile)
-        thisImg = HDUList['SCI'].data
+        wlCube = np.zeros([self.nImg,y2-y1,x2-x1])
         
-        wlCube[ind] = thisImg[y1:y2,x1:x2]
+        for ind in tqdm.tqdm(np.arange(self.nImg)):
+            oneFile = self.fileList[ind]
+            
+            HDUList = fits.open(oneFile)
+            thisImg = HDUList['SCI'].data
+            
+            wlCube[ind] = thisImg[y1:y2,x1:x2]
+            HDUList.close()
+            
         
+        outName = os.path.basename(self.firstPath).replace('.fits','_WL_cube.fits')
+        
+        HDUList_out = fits.PrimaryHDU(wlCube,self.firstHead)
+        HDUList_out.writeto(os.path.join(self.outDir,outName),overwrite=True)
+
+    def run_all(self):
+        self.make_WL_cube()
     
-    outName = os.path.basename(firstPath).replace('.fits','_WL_cube.fits')
-    
-    HDUList_out = fits.PrimaryHDU(wlCube,firstHead)
-    HDUList_out.writeto(os.path.join(outDir,outName),overwrite=True)
+
+def make_WL_cube(fileSearch=defFileSearch):
+    wlCubeObj = wlCubeMake()
+    wlCubeObj.run_all()
